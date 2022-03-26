04465ccceb446c97ebd8a57a1d2f27fd244dd8a5
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2022-03-26 14:45:21 +0900

    refactor: extract functions to modules

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
+pub mod constants;
+pub mod table_reader;
+pub mod formatter;
---
+++ src/table_reader.rs
@@ -0,0 +1,24 @@
+use csv::ReaderBuilder;
+
+pub fn split_csv_line(li: usize, line: &str) -> Vec<String> {
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b',')
+        .has_headers(false)
+        .from_reader(line.as_bytes());
+    if let Some(result) = rdr.records().next() {
+        let record = result.unwrap_or_else(|_e| {
+           eprintln!("Error: line {}: invalid text: {}", li + 1, line);
+           std::process::exit(1);
+        });
+        let v: Vec<String> = record.iter().map(|item| item.to_string()).collect();
+        v
+    } else {
+        unreachable!();
+    }
+}
+
+pub fn split_tsv_line(_li: usize, line: &str) -> Vec<String> {
+    let v: Vec<String> = line.split('\t').map(|item| item.to_string()).collect();
+    v
+}
+
